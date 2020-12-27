e158c9e1a397fa6732fd22223e62c82b7e2e4210
mahdij <mahdimn2011@yahoo.com>
2020-12-28 00:27:41 +0330

    slf4j added for logging

--- src/main/java/com/example/project/controller/BusinessController.java
+++ src/main/java/com/example/project/controller/BusinessController.java
@@ -4,6 +4,7 @@ import com.example.project.model.business.BusinessDTO;
 import com.example.project.service.business.IBusinessService;
 import com.example.project.service.location.ILocationService;
 import com.example.project.utils.URLUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RequestMapping(URLUtils.BUSINESS)
 @Controller
 public class BusinessController extends BaseController {
