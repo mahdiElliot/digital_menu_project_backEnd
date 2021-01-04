037fa0d9c82fd4e6cbefec48c8c8cd1e319badc4
mahdij <mahdimn2011@yahoo.com>
2021-01-05 00:19:29 +0330

    option image handled

--- web/src/main/java/com/example/project/controller/OptionController.java
+++ web/src/main/java/com/example/project/controller/OptionController.java
@@ -6,11 +6,17 @@ import com.example.project.model.option.OptionDTO;
 import com.example.project.service.business.IBusinessService;
 import com.example.project.service.extra.IExtraService;
 import com.example.project.service.option.IOptionService;
+import com.example.project.utils.FileUploadUtil;
 import com.example.project.utils.URLUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.function.Function;
 
 @RestController
@@ -35,7 +41,12 @@ public class OptionController extends BaseController {
 
     @PostMapping(URLUtils.OPTION)
     @ResponseStatus(HttpStatus.CREATED)
-    public OptionDTO addOption(@RequestBody OptionDTO optionDTO) {
-        return optionService.save(optionDTO.convertToOptionEntity(extraMapper()));
+    public OptionDTO addOption(@Valid OptionDTO optionDTO, @RequestParam("photo") MultipartFile multipartFile) throws IOException {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        String uploadDir = URLUtils.OPTION + "/photos/";
+        optionDTO.setImage(uploadDir + fileName);
+        OptionDTO optionDTO2 = optionService.save(optionDTO.convertToOptionEntity(extraMapper()));
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        return optionDTO2;
     }
 }
