c753808c1b2f2059bf69c7b504c19c14ea498638
mahdij <mahdimn2011@yahoo.com>
2021-01-05 16:06:47 +0330

    deployment, war generated

--- web/src/main/java/com/example/project/utils/FileUploadUtil.java
+++ web/src/main/java/com/example/project/utils/FileUploadUtil.java
@@ -15,7 +15,8 @@ public class FileUploadUtil {
     public static void saveFile(String uploadDir, String fileName,
                                 MultipartFile multipartFile) throws IOException {
         String DIR = "web/src/main/resources/public";
-        Path uploadPath = Paths.get(DIR + uploadDir);
+//        Path uploadPath = Paths.get(DIR + uploadDir);
+        Path uploadPath = Paths.get(uploadDir);
 
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
