43ce9315ad8964c6d31a9ed02ef4b9cd55ea9d85
mahdij <mahdimn2011@yahoo.com>
2021-01-23 22:07:40 +0330

    some bugs about images fixed

--- web/src/main/java/com/example/project/config/WebConfig.java
+++ web/src/main/java/com/example/project/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
